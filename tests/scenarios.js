9b04332bc03f902ead647fb1e729b8a13c54a26a
Danilo Batista de Souza <danilo.souza@loglabdigital.com.br>
2025-02-02 11:53:48 -0300

    perform scenario with custom-metrics

--- tests/scenarios.js
+++ tests/scenarios.js
@@ -2,11 +2,16 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 1,
-    duration: '1s',
+    vus: 10,
+    duration: '10s',
     thresholds: {
-        http_req_duration: ['p(95)<200'],
-        http_req_failed: ['rate<0.01']
+        http_req_duration: ['p(95)<500']['max<500'],
+        //http_req_duration: ['max<500'],
+        http_req_failed: ['rate<0.01'],
+        //http_reqs: ['count>20'],
+        http_reqs: ['rate>4'],
+        vus: ['value>9'],
+        checks: ['rate>=0.98']
     }
 };
 
