c782bf1b66ccce786f2782d36a3019ad48dafe7e
Danilo Batista de Souza <danxsouza@proton.me>
2025-02-15 14:31:42 -0300

    refactoring http-post to use dynamic credentials

--- tests/http-post.js
+++ tests/http-post.js
@@ -2,23 +2,32 @@ import http from 'k6/http';
 import { check }  from 'k6';
 
 export default function () {
+
+    const credentials = {
+        username: 'test_' + Date.now(),
+        password: 'secret_' + Date.now(),
+    }
+
     const body = JSON.stringify({
         username: 'test_' + Date.now(),
         password: 'test_9900239020990320'
     });
 
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
-   http.post('https://test-api.k6.io/user/register/', body, params);
+   http.post(
+       'https://test-api.k6.io/user/register/',
+       JSON.stringify(credentials),
+       {
+           headers: {
+               'Content-Type': 'application/json',
+           },
+       }
+   );
+   
    let res = http.post(
        'https://test-api.k6.io/auth/token/login/',
        JSON.stringify({
-           username: 'test11',
-           password: 'test11',
+           username: credentials.username,
+           password: credentials.password
        }),
        {
            headers: {
